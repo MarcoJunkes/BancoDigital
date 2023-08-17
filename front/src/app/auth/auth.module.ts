4dacbe87ad78756b3fd4eb8fb6b199165db2591e
Matheus Reis <theus.h.ass.reis@gmail.com>
2023-08-17 20:43:18 -0300

    create login and cadastro screens

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    CadastroComponent
+  ],
   imports: [
     CommonModule
   ]
