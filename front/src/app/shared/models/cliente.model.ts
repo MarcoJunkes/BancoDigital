e82b80b52a45075068b8e47fd6b89194a4dcc258
Tiago_shigueo <tiago.yamasaki@gmail.com>
2023-09-14 18:22:48 -0300

    [ADD] Json na tela inicial de gerente

--- front/src/app/shared/models/cliente.model.ts
+++ front/src/app/shared/models/cliente.model.ts
@@ -9,6 +9,7 @@ export class Cliente {
         // Dados daqui para baixo são da conta. Apagar para implementar os microsserviços
         public dataCriacao?: Date,
         public limite?: number,
-        public gerente?: string
+        public gerente?: string,
+        public salario?: number
     ){}
 }
