d380d6190cfd17981bb9cefbe0c77fae06dcb32c
Matheus Reis <theus.h.ass.reis@gmail.com>
2023-09-23 11:09:47 -0300

    fix: transferencia model

--- front/src/app/cliente/transferencia/transferencia.component.ts
+++ front/src/app/cliente/transferencia/transferencia.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Transferencia } from 'src/app/shared';
 
 @Component({
   selector: 'app-transferencia',
   templateUrl: './transferencia.component.html'
 })
-export class TransferenciaComponent {
+export class TransferenciaComponent implements OnInit {
+  public transferencia!: Transferencia;
+  @ViewChild('transferenciaForm') transferenciaForm!: NgForm;
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.transferencia = new Transferencia();
+  }
+
+  transferir(): void {
+    console.log(this.transferencia);
+  }
+
+  voltar(): void {
+    console.log('voltar');
+  }
 }
