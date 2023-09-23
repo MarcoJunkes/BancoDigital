e753bccbbc15be10bbbc5d82464ebfe43390550d
Matheus Reis <theus.h.ass.reis@gmail.com>
2023-09-23 12:55:07 -0300

    fix: deposito and saque html

--- front/src/app/cliente/deposito/deposito.component.ts
+++ front/src/app/cliente/deposito/deposito.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Deposito } from 'src/app/shared';
 
 @Component({
   selector: 'app-deposito',
   templateUrl: './deposito.component.html'
 })
 export class DepositoComponent {
+  public deposito!: Deposito;
+  @ViewChild('formDeposito') formDeposito!: NgForm;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.deposito = new Deposito();
+  }
 
 }
