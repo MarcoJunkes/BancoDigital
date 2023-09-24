84fe7a4c09739e852a67d8981b95f0dbe76fbc62
Matheus Reis <theus.h.ass.reis@gmail.com>
2023-09-24 16:44:06 -0300

    fix: add deposito

--- front/src/app/cliente/deposito/deposito.component.ts
+++ front/src/app/cliente/deposito/deposito.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Deposito } from 'src/app/shared';
+import { Deposito, OperacaoDirecao, OperacaoTipo } from 'src/app/shared';
+import { OperacaoService } from '../services/operacao.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-deposito',
@@ -10,10 +12,15 @@ export class DepositoComponent {
   public deposito!: Deposito;
   @ViewChild('formDeposito') formDeposito!: NgForm;
 
-  constructor() { }
+  constructor(private readonly operacaoService: OperacaoService, private router: Router) { }
 
   ngOnInit(): void {
-    this.deposito = new Deposito();
+    this.deposito = new Deposito(OperacaoTipo.DEPOSITO, new Date().toISOString().split('T')[0], OperacaoDirecao.ENTRADA);
   }
 
+  depositar(): void {
+    this.operacaoService.salvar(this.deposito).subscribe(
+      () => this.router.navigate(['/cliente/home'])
+    );
+  }
 }
