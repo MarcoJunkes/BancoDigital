7769b358fa4b98117c8a6bb2ade5a5ab9ce21690
Matheus Reis <theus.h.ass.reis@gmail.com>
2023-10-30 00:44:55 -0300

    add mongodb code

--- back/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ back/auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -1,13 +1,24 @@
 package com.example.authservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private CustomerService customerService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("opa");
+        customerService.save("Matheus", "Reis");
+    }
 }
