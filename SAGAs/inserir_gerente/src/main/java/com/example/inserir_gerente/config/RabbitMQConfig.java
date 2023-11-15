e38395e8a9f47989e4e0dd9a5297ae8f9f0c9735
MarcoJunkes <marcojunkes@ufpr.br>
2023-11-15 19:28:53 -0300

    Geração automática de fila

--- SAGAs/inserir_gerente/src/main/java/com/example/inserir_gerente/config/RabbitMQConfig.java
+++ SAGAs/inserir_gerente/src/main/java/com/example/inserir_gerente/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.example.inserir_gerente.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +10,15 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class RabbitMQConfig {
-    
+
+    @Value("service_gerente__request_inserir_gerente")
+    private String queue;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
     @Bean
     public ObjectMapper objectMapper(){
         return JsonMapper.builder().findAndAddModules().build();
