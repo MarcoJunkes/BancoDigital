bb5d0e606b9855c481f84fe7b01294dad162aa3f
MarcoJunkes <marcojunkes@ufpr.br>
2023-11-25 20:30:58 -0300

    [FIX] ajustado o config

--- SAGAs/remover_gerente/src/main/java/com/example/remover_gerente/config/RabbitMQConfig.java
+++ SAGAs/remover_gerente/src/main/java/com/example/remover_gerente/config/RabbitMQConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -30,5 +31,12 @@ public class RabbitMQConfig {
     @Bean
     public ObjectMapper objectMapper(){
         return JsonMapper.builder().findAndAddModules().build();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setCreateMessageIds(true);
+        return converter;
     }
 }
